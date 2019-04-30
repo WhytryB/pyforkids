e2c958db2b800a1d7dde37730343e3fc7fcf61e8
whytryb <offlittlehooligans@gmail.com>
2019-05-01 01:28:34 +0300

    update settngs

--- freelance/settings.py
+++ freelance/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$29y#6wz@c-os=grnt=_40fjn&-cotyxk(dhpw)_=z_mtfp(n$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-SECRET_KEY = ""
+SECRET_KEY = "2b1e0efd-bf60-473b-a975-74caa7c252da"
 
 ALLOWED_HOSTS = []
 
